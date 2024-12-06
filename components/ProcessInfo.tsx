1bcd4a1e02cfe07fae9aaa9481b3261856779cdd
Will Kim <rlagmlckd@gmail.com>
2024-12-06 12:15:58 +0900

    이용 프로세스 안내 Tablet 업데이트

--- components/ProcessInfo.tsx
+++ components/ProcessInfo.tsx
@@ -28,23 +28,23 @@ export default function ProcessInfo() {
   return (
     <>
       <h2 className="mt-[64px] mb-[24px]">이용 프로세스 안내</h2>
-      <ol className="flex">
+      <ol className="flex flex-col lg:flex-row space-y-[24px] lg:space-y-0">
         {PROCESS_INFO.map(({ icon, title, description }, i) => {
           return (
             <li
-              className="mx-[16px] px-[24px] w-full flex flex-col relative"
+              className="mx-[16px] lg:px-[24px] w-full flex lg:flex-col relative space-x-[16px] lg:space-x-0"
               key={uuidv4()}
             >
               <i
-                className={`${icon} w-[56px] h-[56px] aspect-square bg-no-repeat bg-contain mb-[8px]`}
+                className={`${icon} w-[48px] lg:w-[56px] h-[48px] lg:h-[56px] aspect-square bg-no-repeat bg-contain lg:mb-[8px]`}
               ></i>
               <span
-                className={`flex flex-col ${
+                className={`flex flex-col text-[18px] lg:text-[16px] ${
                   i === 0 ? "before:hidden" : ""
-                } before:content-[''] before:absolute before:bg-[url('/ic_step_arrow.svg')] before:bg-no-repeat before:bg-contain before:h-[24px] before:w-[24px] before:-left-4`}
+                } before:hidden before:lg:inline-block before:content-[''] before:absolute before:bg-[url('/ic_step_arrow.svg')] before:bg-no-repeat before:bg-contain before:h-[24px] before:w-[24px] before:-left-4`}
               >
                 <strong>{title}</strong>
-                <em className="text-[16px] not-italic font-normal text-[#36434c] mt-[8px]">
+                <em className="text-[16px] leading-[22.4px] lg:leading-[24px] not-italic font-normal text-[#36434c] mt-[8px]">
                   {description}
                 </em>
               </span>
