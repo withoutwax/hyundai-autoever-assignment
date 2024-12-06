349152d83d8290e04a1a94e1f46133513fd8d0c5
Will Kim <rlagmlckd@gmail.com>
2024-12-07 04:59:22 +0900

    위로 스크롤 버트 Z-index 재설정

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -8,7 +8,7 @@ export default function ScrollToTopButton() {
   const queryClient = useQueryClient();
 
   return (
-    <div className="sticky bottom-0 left-0 w-full z-[99] pointer-events-none block">
+    <div className="sticky bottom-0 left-0 w-full pointer-events-none block z-30">
       <div className="absolute right-[34px] bottom-[40px] flex items-center flex-col pointer-events-none ">
         <button
           className={`bg-[url('/ic_top.svg')] w-[56px] h-[56px] mt-[8px] bg-[28px_auto] bg-no-repeat  text-[0px] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.12)] rounded-full pointer-events-auto bg-white ${
