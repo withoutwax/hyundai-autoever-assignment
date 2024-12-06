2f838002b68174e54ce104eb69c646834f1436a8
Will Kim <rlagmlckd@gmail.com>
2024-12-07 04:58:44 +0900

    커스텀 Media breakpoint 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      md: "744px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
