6cc9bb482bd4c9a322b901e5629a579ee5ea53eb
Will Kim <rlagmlckd@gmail.com>
2024-12-07 05:39:49 +0900

    비어있는 페이지 구축

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function News() {
   return (
     <div className="flex flex-col text-center justify-center items-center min-h-screen space-y-10">
       <h1 className="text-4xl">페이지가 비어있습니다</h1>
