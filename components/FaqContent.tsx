163ff62135e115d24d7fbae4ca1a43da88787076
Will Kim <rlagmlckd@gmail.com>
2024-12-06 17:23:21 +0900

    FAQ 1차 업데이트

--- components/FaqContent.tsx
+++ components/FaqContent.tsx
@@ -41,7 +41,7 @@ export default function FaqContent({ data }: { data: FaqContentProps[] }) {
               activeCategory === "전체"
                 ? "text-white bg-mint rounded-[24px]"
                 : "text-midnight"
-            } not-italic flex items-center px-[20px] h-[48px]  text-[18px] font-semibold`}
+            } not-italic flex items-center px-[14px] lg:px-[20px] h-[44px] lg:h-[48px] text-[16px] lg:text-[18px] font-semibold`}
           >
             전체
           </i>
@@ -69,7 +69,7 @@ export default function FaqContent({ data }: { data: FaqContentProps[] }) {
                 activeCategory === category
                   ? "text-white bg-mint rounded-[24px]"
                   : "text-midnight"
-              } not-italic flex items-center px-[20px] h-[48px]  text-[18px] font-semibold`}
+              } not-italic flex items-center px-[14px] lg:px-[20px] h-[44px] lg:h-[48px] text-[16px] lg:text-[18px] font-semibold`}
             >
               {category}
             </i>
@@ -87,26 +87,31 @@ export default function FaqContent({ data }: { data: FaqContentProps[] }) {
             <Accordion.Item key={uuid} value={uuid}>
               <Accordion.Header className="relative group">
                 <Accordion.Trigger
-                  className="border-b border-[#e6e8e9] w-full flex items-center p-[16px] text-midnight font-semibold text-[20px] py-[24px] 
+                  className="border-b border-[#e6e8e9] w-full flex flex-col lg:flex-row items-start lg:items-center justify-center lg:justify-start pl-[0px] lg:px-[16px] text-midnight font-semibold text-[20px] py-[24px] 
             data-[state=open]:bg-[#f8f8f8]
-            after:content-[''] after:absolute after:bg-[url('/ic_arrow.svg')] after:bg-no-repeat after:bg-[auto_100%] after:right-[22px] after:w-[32px] after:h-[32px]  
+            after:content-[''] after:absolute after:bg-[url('/ic_arrow.svg')] after:bg-no-repeat after:bg-[auto_100%] after:right-[22px] after:w-[24px] after:lg:w-[32px] after:h-[24px] after:lg:h-[32px]  
             after:transition-transform after:ease-custom after:duration-300 group-data-[state=open]:after:rotate-180
           "
                 >
-                  <em className="w-[208px] not-italic text-center px-[24px] text-[#697278] font-normal">
-                    {faq.category}
-                  </em>
-                  {faq.subCategory && (
-                    <em className="w-[208px] not-italic text-center px-[24px] text-[#697278] font-normal">
-                      {faq.subCategory}
+                  <div className="flex flex-row lg:flex-col items-center lg:items-start">
+                    <em className="lg:w-[208px] not-italic text-center lg:px-[24px] text-[#697278] font-normal text-[16px] lg:text-[20px] mb-[4px] lg:mb-0">
+                      {faq.category}
                     </em>
-                  )}
+                    {faq.subCategory && (
+                      <em
+                        className="lg:w-[208px] not-italic text-center lg:px-[24px] text-[#697278] font-normal text-[16px] lg:text-[20px] mb-[4px] lg:mb-0 flex items-center
+                                   before:lg:hidden before:content-[''] before:inline-block before:bg-[url('/ic_step_arrow.svg')] before:bg-no-repeat before:bg-contain before:h-[12px] before:w-[12px] before:mx-2"
+                      >
+                        {faq.subCategory}
+                      </em>
+                    )}
+                  </div>
                   <strong className="flex-1 text-left">{faq.question}</strong>
                 </Accordion.Trigger>
               </Accordion.Header>
               <Accordion.Content className="border-b border-[#e6e8e9] duration-[100ms] transition-[max-height] ease-in-out h-full overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
-                <div className="px-[40px] py-[32px]">
-                  <p className="whitespace-pre-line text-[18px] text-[#697278] leading-[1.8em]">
+                <div className="p-[24px] lg:px-[40px] lg:py-[32px]">
+                  <p className="whitespace-pre-line text-[#697278] leading-[1.8em] text-[16px] lg:text-[18px]">
                     {faq.answer}
                   </p>
                 </div>
@@ -121,7 +126,7 @@ export default function FaqContent({ data }: { data: FaqContentProps[] }) {
           onClick={() => {
             setLimit(limit + 10);
           }}
-          className="text-[20px] w-full text-center my-10"
+          className="text-[16px] lg:text-[20px] w-full text-center my-10"
         >
           +더보기
         </button>
