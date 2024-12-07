a9e9632feadb63c17aa3f8b9965f7cb7a94ce305
Will Kim <rlagmlckd@gmail.com>
2024-12-08 03:57:48 +0900

    SEO 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,31 @@ const kiaSignatureFix = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "서비스 도입 FAQ | 위블 비즈(Wible Biz) - 친환경 모빌리티 서비스",
+  title: "서비스 이용 FAQ | 위블 비즈(Wible Biz) - 친환경 모빌리티 서비스",
   description:
     "위블 비즈는 기업을 위한 친환경 모빌리티 서비스로 차량부터 전용 App/Web까지 업무차량 토탈 솔루션을 제공합니다.",
+  metadataBase: new URL("https://wiblebiz.kia.com"),
+  keywords:
+    "위블비즈,위블 비즈,모빌리티,구독서비스,차량구독,차량관리,업무용차량,법인차,관용차,전기차,FMS,스마트솔루션",
+  themeColor: "#000000",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://wiblebiz.kia.com",
+    title: "위블 비즈(Wible Biz)",
+    description:
+      "위블 비즈는 기업을 위한 친환경 모빌리티 서비스로 차량부터 전용 App/Web까지 업무차량 토탈 솔루션을 제공합니다.",
+    siteName: "위블 비즈(Wible Biz)",
+    images: [
+      {
+        url: "/wb_sns_default.jpg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    images: "/wb_sns_default.jpg",
+  },
 };
 
 export default function RootLayout({
