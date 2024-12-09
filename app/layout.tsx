47262a8b5f521aa2a73b7ba4b27ca4216bda9241
Will Kim <rlagmlckd@gmail.com>
2024-12-09 17:43:52 +0900

    metaData - themeColor 없음

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://wiblebiz.kia.com"),
   keywords:
     "위블비즈,위블 비즈,모빌리티,구독서비스,차량구독,차량관리,업무용차량,법인차,관용차,전기차,FMS,스마트솔루션",
-  themeColor: "#000000",
   openGraph: {
     type: "website",
     locale: "ko_KR",
